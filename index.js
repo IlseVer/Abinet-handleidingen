11d6434bd418a1bba7268040fd525336e2b3bdd8
Ilse Verhaeghe <115065921+IlseVer@users.noreply.github.com>
2025-11-08 16:29:29 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Server werkt! 🚀");
-});
-
-app.listen(3000, () => console.log("Server draait op http://localhost:3000"));
