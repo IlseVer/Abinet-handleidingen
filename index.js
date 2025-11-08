c070faa4f17a3dd50060c17337c8ae42b94c2f29
Ilse Verhaeghe <115065921+IlseVer@users.noreply.github.com>
2025-11-08 15:26:19 +0000

    node.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server werkt! 🚀");
+});
+
+app.listen(3000, () => console.log("Server draait op http://localhost:3000"));
